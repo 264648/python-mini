5207131a4318d927f104d6224a7275836d311e02
264648 <81163246+264648@users.noreply.github.com>
2021-07-07 14:31:35 +0530

    Create calc.h

---
+++ inc/calc.h
@@ -0,0 +1,47 @@
+/**
+ *@file calc.h
+ *@author Sai Rishitha S
+ *@brief
+ *@version 0.1
+ *@date 2021-07-07
+ *
+ *@copyright Copyright (c) 2021
+ *
+ */
+#ifndef _CALC_H
+#define _CALC_H
+/**
+ *@brief calc file which includes all
+ *
+ */
+def add():
+/**
+ *@brief performs addition operation
+ *
+ */
+def sub():
+/**
+ *@brief performs subtraction operation
+ *
+ */
+def mul():
+/**
+ *@brief performs multiplication operation
+ *
+ */
+def div():
+/**
+ *@brief performs division operation
+ *
+ */
+def mod():
+/**
+ *@brief performs modulus operation
+ *
+ */
+def pow():
+/**
+ *@brief performs power of a number operation
+ *
+ */
+# endif
